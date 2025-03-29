6a8eb2b1b3e5fc87b0d65afeacacd26a5bb63329
薛困惑 <xue7106211@gmail.com>
2025-03-29 18:46:42 +0800

    解决vercel部署问题

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,18 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'gsap-vendor': ['gsap', '@gsap/react']
+        }
+      }
+    }
   }
 }) 
